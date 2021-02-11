2deef94927dabde07c30ec975b8e373572097178
Alena Gontarenko <gontarenko.alena@gmail.com>
2021-02-11 13:32:18 +0200

    add auth and migration

---
+++ database/migrations/2021_02_11_112540_alter_table_users.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->tinyInteger('is_admin')->default(0);
+            $table->tinyInteger('is_banned')->default(0);
+            $table->text('image')->nullable()->after('password');
+            $table->string('tel')->nullable()->after('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('is_admin');
+            $table->dropColumn('is_banned');
+            $table->dropColumn('image');
+            $table->dropColumn('tel');
+        });
+    }
+}
